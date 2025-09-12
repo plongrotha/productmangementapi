1dc042cb658f859c4dede1a375dfde8a795a3b0f
plongrotha <Mrr.rothabetta31@gmail.com>
2025-09-12 12:36:21 +0700

    develop: added to dev

--- src/main/java/com/productmanagement/productmanagementapi/config/CacheConfig.java
+++ src/main/java/com/productmanagement/productmanagementapi/config/CacheConfig.java
@@ -17,7 +17,6 @@ public class CacheConfig {
 
     @Bean
     CacheManager cacheManager() {
-
         CaffeineCacheManager cacheManager = new CaffeineCacheManager();
         cacheManager.registerCustomCache("user", Caffeine.newBuilder().expireAfterWrite(10, TimeUnit.HOURS)
                 .maximumSize(500)
