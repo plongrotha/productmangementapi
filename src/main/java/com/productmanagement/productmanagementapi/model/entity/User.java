7e53e8d81b6c9e9899173a061a1556ae3556c072
plongrotha <Mrr.rothabetta31@gmail.com>
2025-09-18 14:59:07 +0700

    add field to class user

--- src/main/java/com/productmanagement/productmanagementapi/model/entity/User.java
+++ src/main/java/com/productmanagement/productmanagementapi/model/entity/User.java
@@ -1,6 +1,55 @@
 package com.productmanagement.productmanagementapi.model.entity;
 
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
 public class User {
 
-}
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private long userId;
 
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "full_name")
+    private String fullname;
+
+    @Column(name = "usename")
+    private String userName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "phone")
+    private String phoneNumber;
+
+    @Column(name = "date_of_birth")
+    private LocalDate dateOfBirth;
+
+    @Column(name = "age")
+    private int age;
+}
